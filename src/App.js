dfab39a7b2d4c3bd55b4a47e2e783ab72968e1da
inzidang <inzidang@naver.com>
2023-04-04 16:35:20 +0900

    안녕

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ function App() {
    <div>
      <div>Hello React</div>
      <h2>react stydy</h2>
-   <table>
+   <table border="1">
     <tr>
       <td>시간</td>
       <td>과목</td>
+    </tr>
+    <tr>
+      <td>06:00~12:00</td>
+      <td>html+CSS</td>
     </tr>
    </table>
    </div>
